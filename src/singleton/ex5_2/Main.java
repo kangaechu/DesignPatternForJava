48537a5550f4bc9afb0811799735d7aae19c2cd0
kangaechu <kangae2@gmail.com>
2020-07-13 19:49:09 +0900

    練習問題5-2

---
+++ src/singleton/ex5_2/Main.java
@@ -0,0 +1,10 @@
+package singleton.ex5_2;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            Triple triple = Triple.getInstance(i%3);
+            System.out.println(i + ":" + triple);
+        }
+    }
+}
---
+++ src/singleton/ex5_2/Triple.java
@@ -0,0 +1,26 @@
+package singleton.ex5_2;
+
+public class Triple {
+    private static Triple[] triples = new Triple[]{
+            new Triple(0),
+            new Triple(1),
+            new Triple(2)
+    };
+    private int id;
+
+    private Triple(int id) {
+        this.id = id;
+    }
+
+    public static Triple getInstance(int id) {
+        return triples[id];
+    }
+
+    @Override
+    public String toString() {
+        return "Triple{" +
+                "id=" + id +
+                '}';
+    }
+}
+
